1e5a38cc1f0050b7d16381799c350afd192f1603
lina98st <156576799+lina98st@users.noreply.github.com>
2026-05-11 09:33:50 +0200

    fix: center header title and login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import UserLoginForm from './UserLoginForm';
 
 const Header = () => {
     return (
-        <header className="d-flex justify-content-between align-items-center px-4 py-3">
+        <header className="d-flex flex-column align-items-center py-3 px-4">
             <h1>CocktailFinder</h1>
             <UserLoginForm />
         </header>
