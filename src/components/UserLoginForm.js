abecded6c6a610746c959b649de75e2a8cf870bd
lina98st <156576799+lina98st@users.noreply.github.com>
2026-05-11 09:15:49 +0200

    refactor: replace CSS with Bootstrap classes in UserLoginForm

--- src/components/UserLoginForm.js
+++ src/components/UserLoginForm.js
@@ -20,9 +20,9 @@ const UserLoginForm = () => {
     return (
         <>
             {currentUser ? (
-                <img src={currentUser.avatar} alt="user" className="user-avatar" />
+                <img src={currentUser.avatar} alt="user" className="user-avatar rounded-circle" />
             ) : (
-                <button className="login-button" onClick={() => setLoginModalOpen(true)}>
+                <button className="btn login-button" onClick={() => setLoginModalOpen(true)}>
                     Login
                 </button>
             )}
@@ -30,9 +30,9 @@ const UserLoginForm = () => {
             {loginModalOpen && (
                 <div className="modal-overlay">
                     <div className="modal-box">
-                        <div className="modal-header">
-                            <h5>Login</h5>
-                            <button onClick={() => setLoginModalOpen(false)}>X</button>
+                        <div className="modal-header d-flex justify-content-between align-items-center">
+                            <h5 className="mb-0">Login</h5>
+                            <button className="btn-close btn-close-white" onClick={() => setLoginModalOpen(false)} />
                         </div>
                         <div className="modal-body">
                             <Formik
@@ -42,20 +42,20 @@ const UserLoginForm = () => {
                             >
                                 <Form>
                                     <div className="mb-3">
-                                        <label htmlFor="username">Username</label>
+                                        <label htmlFor="username" className="form-label">Username</label>
                                         <Field id="username" name="username" placeholder="Username" className="form-control" />
                                         <ErrorMessage name="username">
-                                            {(msg) => <p className="text-danger">{msg}</p>}
+                                            {(msg) => <p className="text-danger small mt-1">{msg}</p>}
                                         </ErrorMessage>
                                     </div>
                                     <div className="mb-3">
-                                        <label htmlFor="password">Password</label>
+                                        <label htmlFor="password" className="form-label">Password</label>
                                         <Field id="password" name="password" placeholder="Password" className="form-control" type="password" />
                                         <ErrorMessage name="password">
-                                            {(msg) => <p className="text-danger">{msg}</p>}
+                                            {(msg) => <p className="text-danger small mt-1">{msg}</p>}
                                         </ErrorMessage>
                                     </div>
-                                    <button className="modal-submit-button" type="submit">Sign In</button>
+                                    <button className="btn modal-submit-button w-100" type="submit">Sign In</button>
                                 </Form>
                             </Formik>
                         </div>
