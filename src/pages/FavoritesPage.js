4929f140afde30fbed0abf301291edb7633da829
lina98st <156576799+lina98st@users.noreply.github.com>
2026-05-15 22:17:46 +0200

    fix: disable eslint exhaustive-deps for FavoritesPage

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { getFavorites } from '../api/favorites';
 import CocktailList from '../components/CocktailList';
