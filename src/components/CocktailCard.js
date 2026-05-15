45881b89b6a02b59ce23c8b5942b2bd16ad03641
lina98st <156576799+lina98st@users.noreply.github.com>
2026-05-15 17:22:02 +0200

    feat: add favorite button to CocktailCard

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { addFavorite } from '../api/favorites';
 
 const CocktailCard = ({ cocktail, deleteCocktail }) => {
+const [isFavorite, setIsFavorite] = useState(false);
+
+    const addToFavorite = async () => {
+        const token = localStorage.getItem('token');
+
+    await addFavorite(token, cocktail.idDrink);
+        setIsFavorite(true)
+    };
+
+
   return (
     <div className="cocktail-card">
       <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} className="img-fluid rounded" />
@@ -11,6 +23,7 @@ const CocktailCard = ({ cocktail, deleteCocktail }) => {
           <button className="btn btn-cocktail w-100">Details</button>
         </Link>
         <button className="btn btn-danger flex-fill" onClick={() => deleteCocktail(cocktail.idDrink)}>Delete</button>
+         <button className="btn btn-danger flex-fill" onClick={() => addToFavorite()}>Add Favorit</button>
       </div>
     </div>
   );
